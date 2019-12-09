c4969d2fb2bae786d1bb926cbb03ec9e176fa8ee
johan <johan@bareksa.com>
2019-12-09 16:37:55 +0700

    pse

---
+++ app/src/main/java/com/pupr/rumija/Shared/Param.java
@@ -0,0 +1,13 @@
+package com.pupr.rumija.Shared;
+
+public class Param {
+
+    public static final String MAIN_PROTOCOL = "http://";
+
+    public static final String MAIN_URL_PSETK = "warehousename.com/mobile-login";
+
+    public static final String MAIN_COMPLETE_PSETK= MAIN_PROTOCOL+MAIN_URL_PSETK;
+
+
+
+}
